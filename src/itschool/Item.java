457503c431d31260ecbcc3cc44bf0894bf497882
it-school <GITHUB it@osvita.od.ua>
2020-05-21 20:21:39 +0300

    Items list as pricelist added

---
+++ src/itschool/Item.java
@@ -0,0 +1,81 @@
+package itschool;
+
+import java.util.Comparator;
+import java.util.Currency;
+import java.util.Locale;
+
+public class Item
+{
+	public static Comparator<Item> byPriceAsc = (o1, o2) -> (int) (o1.rosnichPrice < o2.rosnichPrice ? -1 : o1.rosnichPrice > o2.rosnichPrice ? 1 : 0);
+	public static Comparator<Item> byPriceDesc = (o1, o2) -> (int) (o1.rosnichPrice > o2.rosnichPrice ? -1 : o1.rosnichPrice < o2.rosnichPrice ? 1 : 0);
+	int id;
+	String title;
+	String sklad;
+	double rosnichPrice;
+	double optPrice;
+	double dilPrice;
+	double gar;
+
+	public Item()
+	{
+		this.id = -1;
+		this.gar = -1;
+		this.dilPrice = -1;
+		this.optPrice = -1;
+		this.rosnichPrice = -1;
+		this.title = "";
+		this.sklad = "";
+	}
+
+
+	public Item(int id, String title, String sklad, double rosnichPrice, double optPrice, double dilPrice, double gar)
+	{
+		this.id = id;
+		this.sklad = sklad;
+		this.rosnichPrice = rosnichPrice;
+		this.optPrice = optPrice;
+		this.gar = gar;
+		this.dilPrice = dilPrice;
+		this.title = title;
+	}
+
+	boolean tryToConvert()
+	{
+		boolean result = false;
+		String tempTitle = this.title.trim();
+        /*if (tempTitle.contains("   ") && tempTitle.contains("ml"))
+        {
+            this.model = tempTitle.substring(tempTitle.indexOf("   "), tempTitle.indexOf(" ",
+                                                                                         tempTitle.indexOf(tempTitle.contains("edt")?"edt":
+                                                                                                                   tempTitle.contains("edp") ? "edp" : "ml"))-3);
+            //this.volume = Integer.parseInt(tempTitle.substring(tempTitle.indexOf("ml")-2, tempTitle.indexOf( "ml")));
+        }
+        */
+
+		return result;
+	}
+
+	@Override
+	public String toString()
+	{
+        /*
+        for (Locale locale : Locale.getAvailableLocales()) {
+            try {
+                Currency currency = Currency.getInstance(locale);
+                System.out.println(locale + " / " + currency);
+            } catch (Exception e) {
+            }
+        }
+*/
+		Locale locale = new Locale("uk_ua", "UA"); //("en", "US");
+		// Locale locale=Locale.getDefault();
+		Currency currency = Currency.getInstance(locale);
+		String symbol = currency.getSymbol();
+
+		return String.format("%10d", id) + "\t" + String.format("%-100s", this.title) + "\t" + this.sklad + "\t" + String.format(
+				"%20.1f", rosnichPrice) + ' ' + symbol + "\t" + String.format(
+				"%20.1f", optPrice) + ' ' + symbol + "\t" + String.format(
+				"%20.1f", dilPrice) + ' ' + symbol + "\t" + String.format(
+				"%10.0f", gar);
+	}
+}
---
+++ src/itschool/Main.java
@@ -0,0 +1,133 @@
+package itschool;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		long start = System.currentTimeMillis();
+
+		Workbook wb = null;
+		String filename = "cmpl.xls";
+		OPCPackage pkg = null;
+		ArrayList<Item> pricelist = new ArrayList<>();
+
+		if (filename.endsWith("xlsx") || filename.endsWith("xls")) {
+			boolean isXLSX = (filename.endsWith("xlsx"));
+
+			try {
+				InputStream inp = new FileInputStream(filename);
+
+				if (isXLSX) {
+					// XSSFWorkbook, File (slower, but uses less memory)
+					pkg = OPCPackage.open(new File(filename));
+
+					// XSSFWorkbook, InputStream, faster, but needs more memory
+					wb = new XSSFWorkbook(OPCPackage.open(inp));
+				}
+				else {
+					wb = new HSSFWorkbook(new POIFSFileSystem(inp));
+
+                /*
+                ExcelExtractor extractor = new ExcelExtractor(wb);
+                extractor.setFormulasNotResults(true);
+                extractor.setIncludeSheetNames(false);
+                String text = extractor.getText();
+                System.out.println(text);
+                */
+				}
+
+				Item item = new Item();
+				Cell cell;
+				int id;
+				String title;
+				String sklad;
+				double rosnichPrice;
+				double optPrice;
+				double dilPrice;
+				double gar;
+
+				for (Sheet sheet : wb) {
+					for (Row row : sheet) {
+                    /*
+                    for (Cell cell : row)
+                    {
+                        //System.out.print(cell + "\t");
+                    }
+                    System.out.println();
+*/
+						cell = row.getCell(0);
+						if (cell == null) {
+							title = row.getCell(1).getStringCellValue();
+							System.out.println(title);
+						}
+						else if (cell != null && cell.getCellType() == CellType.NUMERIC) {
+							id = (int) cell.getNumericCellValue();
+							title = row.getCell(1).getStringCellValue();
+							sklad = row.getCell(2).getStringCellValue();
+							rosnichPrice = row.getCell(3).getNumericCellValue();
+							optPrice = row.getCell(4).getNumericCellValue();
+							dilPrice = row.getCell(5).getNumericCellValue();
+							gar = row.getCell(6).getNumericCellValue();
+							item = new Item(id, title, sklad, rosnichPrice, optPrice, dilPrice, gar);
+							// item.tryToConvert();
+							pricelist.add(item);
+							//System.out.println(item.toString());
+						}
+					}
+				}
+				wb.close();
+
+                /*for (Item item1 : pricelist) {
+                    System.out.println(item1);
+                }*/
+
+				if (pkg != null) {
+					pkg.close();
+				}
+			}
+			catch (FileNotFoundException | InvalidFormatException e) {
+				System.out.println(e.getLocalizedMessage());
+			}
+			catch (IOException e) {
+				System.out.println(e.getLocalizedMessage());
+			}
+
+			long finish = System.currentTimeMillis();
+			System.out.println(finish - start);
+
+			System.out.println("\n\n");
+
+			Items pricelistFull = new Items(pricelist);
+			Items USBlist = pricelistFull.SearchByTitle("USB");
+			System.out.println(USBlist);
+			System.out.println(USBlist.list.size());
+
+			Items sublist2 = pricelistFull.SearchByPriceLowerThan(20);
+			sublist2.sort(Item.byPriceAsc);
+			System.out.println(sublist2);
+			sublist2.sort(Item.byPriceDesc);
+			System.out.println(sublist2);
+
+			Items Logitech_cheaper_200 = pricelistFull.SearchByTitle("Logitech").SearchByPriceLowerThan(200);
+			System.out.println(Logitech_cheaper_200);
+		}
+		else {
+			System.out.println("Given file is NOT Microsoft Excel file!");
+		}
+
+		/*
+		ExcelWriterX writexls = new ExcelWriterX();
+		writexls.run(new String[]{"test.xlsx"});
+		 */
+	}
+}
